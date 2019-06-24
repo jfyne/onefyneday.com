0a3c9c86c3a98d805eaa4af63a8bff0bb9c46fca
Josh Fyne <josh.fyne@essenceglobal.com>
2019-06-23 21:41:36 -0400

    Skeleton for function on save of document

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-//export const rsvp = functions.https.onRequest((request, response) => {
-// response.send("Hello from Firebase!");
-//});
+exports.responseReceievd = functions.firestore
+    .document('responses/{code}')
+    .onWrite((change, context) => {
+      // Get an object with the current document value.
+      // If the document does not exist, it has been deleted.
+      const document = change.after.exists ? change.after.data() : null;
+      // perform desired operations ...
+      console.log(document);
+      return document;
+    });
+
